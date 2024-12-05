b2c17b38eb1276bbd42dacd6c05659839cd770db
Juan Suero <131712448+juansuero@users.noreply.github.com>
2024-12-05 03:31:54 +0000

    Remove unused files and update requirements for Streamlit and Plotly

--- spotify_auth.py
+++ spotify_auth.py
@@ -4,13 +4,24 @@ from spotipy.oauth2 import SpotifyOAuth
 
 CLIENT_ID = '45c01f01bb3447b2bb2c99902d9461c5'
 CLIENT_SECRET = '2ce37e31b5b6411885dd4d8b4d7f9478'
-REDIRECT_URI = 'http://localhost:8888/callback'
-SCOPE = ('user-library-read user-top-read user-read-recently-played '
-         'user-read-playback-state playlist-read-private')
+REDIRECT_URI = 'http://localhost:8501/'  # Streamlit default port
+SCOPE = 'user-top-read user-read-recently-played user-library-read'
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=CLIENT_ID,
-    client_secret=CLIENT_SECRET,
-    redirect_uri=REDIRECT_URI,
-    scope=SCOPE,
-    cache_path='.spotifycache'))
+def create_spotify_oauth():
+    """Create SpotifyOAuth instance for authentication"""
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE,
+        cache_path='.spotifycache'
+    )
+
+def get_token():
+    """Get cached token or create new one"""
+    sp_oauth = create_spotify_oauth()
+    token_info = sp_oauth.get_cached_token()
+    if not token_info:
+        auth_url = sp_oauth.get_authorize_url()
+        return None
+    return token_info
