f28183ef79db53c47536e4e2e2afff3cee307bba
Juan Suero <131712448+juansuero@users.noreply.github.com>
2024-12-05 04:02:10 +0000

    Refactor Spotify authentication to improve secret handling and set default values

--- spotify_auth.py
+++ spotify_auth.py
@@ -10,15 +10,20 @@ def log_debug(msg):
     if DEBUG:
         st.write(f"Debug: {msg}")
 
-# Get deployment URL from Streamlit secrets if available
-if st.secrets and hasattr(st.secrets, "REDIRECT_URI"):
-    REDIRECT_URI = st.secrets.REDIRECT_URI
-    CLIENT_ID = st.secrets.SPOTIFY_CLIENT_ID
-    CLIENT_SECRET = st.secrets.SPOTIFY_CLIENT_SECRET
-else:
-    REDIRECT_URI = os.getenv('REDIRECT_URI', 'http://localhost:8501/')
-    CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID', '45c01f01bb3447b2bb2c99902d9461c5')
-    CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET', '2ce37e31b5b6411885dd4d8b4d7f9478')
+# Default values
+DEFAULT_REDIRECT_URI = 'http://localhost:8501/'
+DEFAULT_CLIENT_ID = '45c01f01bb3447b2bb2c99902d9461c5'
+DEFAULT_CLIENT_SECRET = '2ce37e31b5b6411885dd4d8b4d7f9478'
+
+# Try to get values from secrets or environment
+try:
+    REDIRECT_URI = st.secrets.get("REDIRECT_URI", DEFAULT_REDIRECT_URI)
+    CLIENT_ID = st.secrets.get("SPOTIFY_CLIENT_ID", DEFAULT_CLIENT_ID)
+    CLIENT_SECRET = st.secrets.get("SPOTIFY_CLIENT_SECRET", DEFAULT_CLIENT_SECRET)
+except:
+    REDIRECT_URI = os.getenv('REDIRECT_URI', DEFAULT_REDIRECT_URI)
+    CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID', DEFAULT_CLIENT_ID)
+    CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET', DEFAULT_CLIENT_SECRET)
 
 SCOPE = 'user-top-read user-read-recently-played user-library-read'
 
